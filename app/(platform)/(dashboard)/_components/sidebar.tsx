e587cb2b15a3f806ca4d9a69d6bcf722afb2b1e6
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-11-20 13:08:05 +0400

    add loader to the sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -47,47 +47,55 @@ const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
     }));
   };
 
-  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+  // if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
-  }
+  // }
 
-  return (
-    <>
-      <div className="font-medium text-xs flex items-center mb-1">
-        <span className="pl-4">Workspaces</span>
-        <Button
-          asChild
-          type="button"
-          size="icon"
-          variant="ghost"
-          className="ml-auto"
-        >
-          <Link href="/select-org">
-            <Plus className="h-4 w-4" />
-          </Link>
-        </Button>
-      </div>
-      <Accordion
-        type="multiple"
-        defaultValue={defaultAccordionValue}
-        className="space-y-2"
-      >
-        {userMemberships.data.map(({ organization }) => (
-          <NavItem
-            key={organization.id}
-            isActive={activeOrganization?.id === organization.id}
-            isExpanded={expanded[organization.id]}
-            organization={organization as Organization}
-            onExpand={onExpand}
-          />
-        ))}
-      </Accordion>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="font-medium text-xs flex items-center mb-1">
+  //       <span className="pl-4">Workspaces</span>
+  //       <Button
+  //         asChild
+  //         type="button"
+  //         size="icon"
+  //         variant="ghost"
+  //         className="ml-auto"
+  //       >
+  //         <Link href="/select-org">
+  //           <Plus className="h-4 w-4" />
+  //         </Link>
+  //       </Button>
+  //     </div>
+  //     <Accordion
+  //       type="multiple"
+  //       defaultValue={defaultAccordionValue}
+  //       className="space-y-2"
+  //     >
+  //       {userMemberships.data.map(({ organization }) => (
+  //         <NavItem
+  //           key={organization.id}
+  //           isActive={activeOrganization?.id === organization.id}
+  //           isExpanded={expanded[organization.id]}
+  //           organization={organization as Organization}
+  //           onExpand={onExpand}
+  //         />
+  //       ))}
+  //     </Accordion>
+  //   </>
+  // );
 };
 
 export default Sidebar;
